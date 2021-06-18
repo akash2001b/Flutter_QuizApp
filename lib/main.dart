28cdb6f511b05f3853c74309b7bf3c66b0aeccd9
akash Roy Choudhury <arc.ar602@gmail.com>
2021-06-18 22:04:54 +0530

    first_app

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      "What is your faviorite color?",
+      "what is your favorite animal",
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('This is my first App'),
+          ),
+          body: Column(
+            children: [
+              Text(questions[questionIndex]),
+              RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: answerQuestion,
+              ),
+            ],
+          )),
+    );
+  }
+}
